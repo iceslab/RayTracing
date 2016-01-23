5556fb3379e51ef34ee15086ec68c267d7756a71
Bartosz Cieśla <bartociesla@gmail.com>
2016-01-23 18:40:14 +0100

    Normalizacja i iloczyn skalarny

--- RayTracing/RayTracing/Vector3D.cpp
+++ RayTracing/RayTracing/Vector3D.cpp
@@ -1,1 +1,28 @@
 #include "Vector3D.hpp"
+
+float Vector3D::scalarMul(Vector3D v)
+{
+	return (*this)[0] * v[0] + (*this)[1] * v[1] + (*this)[2] * v[2];
+}
+
+void Vector3D::normalize()
+{
+	float d = 0.0;
+	int i;
+
+	for (i = 0; i < 3; i++)
+		d += (*this)[i] * (*this)[i];
+
+	d = sqrt(d);
+
+	if (d > 0.0)
+	{
+		for (i = 0; i < 3; i++)
+			(*this)[i] /= d;
+	}
+}
+
+float Vector3D::scalarMul(Vector3D p, Vector3D v)
+{
+	return p[0] * v[0] + p[1] * v[1] + p[2] * v[2];
+}
--- RayTracing/RayTracing/Vector3D.hpp
+++ RayTracing/RayTracing/Vector3D.hpp
@@ -1,10 +1,15 @@
 #pragma once
 #include "Wrapper.hpp"
+#include <cmath>
 class Vector3D : public Wrapper
 {
 public:
 	Vector3D() = default;
 	~Vector3D() = default;
 	Vector3D(initializer_list<float> il) :Wrapper(il){};
+
+	float scalarMul(Vector3D v);
+	void normalize();
+	static float scalarMul(Vector3D p, Vector3D v);
 };
 
