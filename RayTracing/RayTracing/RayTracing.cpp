4fadd781e6517c850681fec138571cf5542fde13
Bartosz Cieśla <bartociesla@gmail.com>
2016-01-24 11:41:31 +0100

    Revert "Dobne poprawki"
    This reverts commit 4b28a17f6084defb09e2a2fde214c312ecd04f1a.

--- RayTracing/RayTracing/RayTracing.cpp
+++ RayTracing/RayTracing/RayTracing.cpp
@@ -65,12 +65,13 @@ void RayTracing::TraceFast(Point3D p, Vector3D v)
 	int number = 0;
 	for (int i = 0; i < maxSteps; ++i)
 	{
-
 		number = Intersect(intersPoint, reflectionVector);
 		if (number >= 0) {
 			normalVector = spheres[number].getNormalVector(intersPoint);
-			Reflect(v);
+			Reflect(reflectionVector);
 			color += spheres[number].phong(reflectionVector, lights, intersPoint, global_ambient);
+			
+			//Reflect(v);
 		}
 		else
 			break;
